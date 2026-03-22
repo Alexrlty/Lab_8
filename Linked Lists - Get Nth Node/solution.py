835ac10276a87e9834c44043fca7f3604d239f46
Alexrlty <siu.pn@ucu.edu.ua>
2026-03-22 23:17:32 +0200

    Implement length and get_nth functions for linked list
    Add functions to calculate length and get nth node in a linked list.

--- Linked Lists - Get Nth Node/solution.py
+++ Linked Lists - Get Nth Node/solution.py
@@ -1,1 +1,23 @@
+from preloaded import Node
 
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+
+def lenght(node):
+    num = 0
+    while node:
+        node = node.next
+        num += 1
+    return num
+
+def get_nth(node, index):
+    if not (0 <= index <= lenght(node)-1):
+        raise ValueError
+    for i in range(index):
+        node = node.next
+    return node
+    
+  
