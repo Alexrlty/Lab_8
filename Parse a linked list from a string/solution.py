aa6d50bb4d3e340a90262af34a49472df23b4c15
Alexrlty <siu.pn@ucu.edu.ua>
2026-03-22 18:07:23 +0200

    Add function to create linked list from string
    Implement function to parse a linked list from a string representation.

--- Parse a linked list from a string/solution.py
+++ Parse a linked list from a string/solution.py
@@ -1,1 +1,9 @@
+from preloaded import Node
 
+def linked_list_from_string(list_repr: str) -> Node | None:
+    list_of_pure = list_repr.split(' -> ')[:-1]
+    head = None
+    for data in list_of_pure[::-1]:
+        head = Node(int(data), head)
+    return head
+        
