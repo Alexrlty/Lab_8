0e2e0f7e73532d33118288238ae97e27097c6095
Alexrlty <siu.pn@ucu.edu.ua>
2026-03-23 15:00:59 +0200

    Add remove_duplicates function to linked list
    Implement remove_duplicates function to remove duplicate nodes from a linked list.

--- Linked Lists - Remove Duplicates/solution.py
+++ Linked Lists - Remove Duplicates/solution.py
@@ -1,1 +1,18 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+def remove_duplicates(head):
+    try:
+        copy_head = head
+        if not copy_head.next:
+            return head
+        while copy_head:
+            if copy_head.data == copy_head.next.data:
+                copy_head.next = copy_head.next.next
+            else:
+                copy_head = copy_head.next
+        return head
+    except AttributeError:
+        return head
