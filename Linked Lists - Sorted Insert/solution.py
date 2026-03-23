821e01f0c536e67d73fb208bab56df6091512375
Alexrlty <siu.pn@ucu.edu.ua>
2026-03-23 14:42:15 +0200

    Refactor Node class and add sorted_insert function
    Refactor Node class and implement sorted_insert function.

--- Linked Lists - Sorted Insert/solution.py
+++ Linked Lists - Sorted Insert/solution.py
@@ -1,1 +1,18 @@
+""" For your information:
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+"""
 
+class Node(object):
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+    
+def sorted_insert(head, data):
+    if not head or data < head.data:
+        return Node(data, head)
+    else:
+        head.next = sorted_insert(head.next, data)
+        return head
