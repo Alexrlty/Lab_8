3ff39bc6a1863d5e27dd27a65050a32941bdb3af
Alexrlty <siu.pn@ucu.edu.ua>
2026-03-23 14:43:20 +0200

    Add recursive reverse function for linked list
    Implemented a recursive function to reverse a linked list.

--- Linked Lists - Recursive Reverse/solution.py
+++ Linked Lists - Recursive Reverse/solution.py
@@ -1,1 +1,14 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
+def reverse(head):
+    if not head or not head.next: # search for None / new_head
+        return head
+    new_head = reverse(head.next) # finds last element(new_head)
+    
+    head.next.next = head #  |--> changes next link to current one (2,3)-(3,None) --> (2,None)-(3,2)
+    head.next = None      #  |
+
+    return new_head # saves new head untill the end
