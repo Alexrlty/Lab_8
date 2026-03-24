e3a8c0a93165dd7efd53c12fc0686ac4fd6d5c9d
Alexrlty <siu.pn@ucu.edu.ua>
2026-03-24 15:19:46 +0200

    Add loop_size function for linked list loop detection
    Implement loop_size function to detect loop length in linked list.

--- Can you get the loop?/solution.py
+++ Can you get the loop?/solution.py
@@ -1,1 +1,19 @@
-
+def loop_size(node):
+    slow = node
+    fast = node
+    lenght = 0
+    while node:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            slow = node
+            while node:
+                slow = slow.next
+                fast = fast.next
+                if slow == fast:
+                    start = slow
+                    while node:
+                        fast = fast.next
+                        lenght += 1
+                        if fast == start:
+                            return lenght
